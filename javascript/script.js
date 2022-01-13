ea96fc7437babc31464eb853e5df637dad3180b6
ROEL2407 <roel24komen@gmail.com>
2022-01-13 17:09:52 +0100

    Update: change to fetch function

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,30 @@
-fetch("../json/dataset.json")
-    .then(function (response) {
-        // The JSON data will arrive here
-        return response.json();
-    })
-    .then(function (data) {
-        // Functions will be executed here
-        test(data);
-    })
-    .catch(function (err) {
-        // If an error occured, it will be catched here
-        console.log(err);
-    });
-
-    function test (data) {
-        console.log(data)
+return new Promise((resolve) => {
+    function starters() {
+        // Fetch init API
+        fetch("../json/dataset.json")
+            .then(response => response.json())
+            .then(data => getIngredientData(data))
     }
+
+    function getIngredientData(data) {
+        let itemArray = [];
+            // For each pokemon in results push the pokemon to the array pokemonArray
+            data.forEach(item => {
+                itemArray.push({
+                    name: item.name,
+                    base: item.base,
+                    origin: item.origin
+                });
+            });
+            console.log(data);
+    }
+
+    function getData(url) {
+        // return every fetch
+        return fetch(url)
+            .then(response => response.json())
+            .then(data => data);
+    }
+    starters();
+
+})
