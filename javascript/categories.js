9323ff7c3e958c7657b83dda16b22e9b0ecf6db1
ROEL2407 <roel24komen@gmail.com>
2022-01-16 14:59:03 +0100

    Update: loop over functions to display with innerhtml

--- javascript/categories.js
+++ javascript/categories.js
@@ -1,41 +1,41 @@
+function fetching() {
+    // Fetch init API
+    fetch("../json/categories.json")
+        .then(response => response.json())
+        .then(data => getCatData(data))
+}
 
-    function fetching() {
-        // Fetch init API
-        fetch("../json/categories.json")
-            .then(response => response.json())
-            .then(data => getCatData(data))
-    }
+function getCatData(data) {
+    let catArray = [];
+    // For each pokemon in results push the pokemon to the array pokemonArray
+    data.forEach(item => {
+        catArray.push({
+            name: item.name,
+            class: item.class,
+            functions: item.function
+        });
+    });
+    console.log(catArray);
+    showCategory(catArray);
+}
 
-    function getCatData(data) {
-        let catArray = [];
-            // For each pokemon in results push the pokemon to the array pokemonArray
-            data.forEach(item => {
-                catArray.push({
-                    name: item.name,
-                    class: item.class,
-                    functions: item.function
-                });
-            });
-            console.log(catArray);
-            showCategory(catArray);
-    }
+function showCategory(data) {
+    data.forEach(item => {
+        tempCat = document.createElement('a');
+        tempCat.classList.add(item.class, 'cat-hexagon');
 
-    function showCategory(data) {
-        data.forEach(item => {
-            tempCat = document.createElement('a');
-            tempCat.classList.add(item.class, 'cat-hexagon');
-            
-            tempCat.innerHTML = '<p>' + item.name + '</p>';
-            document.getElementById('background-hex').appendChild(tempCat);
-        })
-    }
+        tempCat.innerHTML = '<p>' + item.name + '</p>'
+        // for (let i = 0; index < item.function.length; i++) {
+        //     '<p>' + item.function[i] +'</p>'
+        // };
+        document.getElementById('background-hex').appendChild(tempCat);
+    })
+}
 
-    function getCats(url) {
-        // return every fetch
-        return fetch(url)
-            .then(response => response.json())
-            .then(data => data);
-    }
-    fetching();
-
-
+function getCats(url) {
+    // return every fetch
+    return fetch(url)
+        .then(response => response.json())
+        .then(data => data);
+}
+fetching();
